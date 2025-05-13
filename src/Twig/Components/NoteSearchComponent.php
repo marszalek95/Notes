d8ba62c88f0a821f7a0137f723697ca6beea1f72
marszalek95 <marszal1995@gmail.com>
2025-05-13 14:44:27 +0200

    created note search component

---
+++ src/Twig/Components/NoteSearchComponent.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Twig\Components;
+
+use App\Repository\NoteRepository;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent(name: 'note_search')]
+final class NoteSearchComponent
+{
+    use DefaultActionTrait;
+
+    #[LiveProp(writable: true)]
+    public string $query = '';
+
+    public function __construct(
+        private readonly NoteRepository $noteRepository,
+    ) {  
+    }
+
+    public function getPosts(): array
+    {
+        return $this->noteRepository->findBySearchQuery($this->query);
+    }
+}
