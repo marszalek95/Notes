fa32b50b427b87e60aa11bc3010f9afcd39a1b71
marszalek95 <marszal1995@gmail.com>
2025-05-20 18:54:46 +0200

    created friendship and sharednote entity

---
+++ src/Entity/Friendship.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Config\FriendshipStatus;
+use App\Repository\FriendshipRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FriendshipRepository::class)]
+class Friendship
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    private ?User $sender = null;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    private ?User $receiver = null;
+
+    #[ORM\Column(enumType: FriendshipStatus::class)]
+    private ?FriendshipStatus $status = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSender(): ?User
+    {
+        return $this->sender;
+    }
+
+    public function setSender(?User $sender): static
+    {
+        $this->sender = $sender;
+
+        return $this;
+    }
+
+    public function getReceiver(): ?User
+    {
+        return $this->receiver;
+    }
+
+    public function setReceiver(?User $receiver): static
+    {
+        $this->receiver = $receiver;
+
+        return $this;
+    }
+
+    public function getStatus(): ?FriendshipStatus
+    {
+        return $this->status;
+    }
+
+    public function setStatus(FriendshipStatus $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
